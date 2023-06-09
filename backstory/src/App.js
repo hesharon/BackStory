1221308c23d9a590a26295a3db81b243e6fc481e
Dynnwa <ddaniel.wei@gmail.com>
2023-06-08 20:42:02 -0700

    Created collections page with fork placeholder

--- backstory/src/App.js
+++ backstory/src/App.js
@@ -1,8 +1,16 @@
-import Home from "./Home";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import Collections from './Collections';
 import "./App.css";
 
 function App() {
-  return <Home />;
-}
-
-export default App;
+  return (<Router>
+    <Routes>
+      <Route exact path="/" element={<Home/>} />
+      <Route path="/collections" element={<Collections/>} />
+    </Routes>
+  </Router>);
+  }
+  
+  export default App;
