4036a4af47960ce442812c901076c7290235a2e7
Dynnwa <ddaniel.wei@gmail.com>
2023-06-13 21:01:28 -0700

    redux, default data, collections page displays

--- backstory/src/components/collection/Collection.js
+++ backstory/src/components/collection/Collection.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Collection.css';
 
-const Collection = ({ images, name }) => (
-  <div className="collection">
+const Collection = ({ images, name, onSelect }) => (
+  <div className="collection" onClick={() => {onSelect()}}>
     <div className="collection-images">
       {images.slice(0, 4).map((img, index) => (
         <div 
           key={index} 
           className="collection-image"
-          style={{ backgroundImage: `url(${img})` }}
+          style={{ backgroundImage: `url(${img.imageSrc})` }}
         ></div>
       ))}
     </div>
