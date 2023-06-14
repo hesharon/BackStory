8b9a943389326b115ec912f12b1af665837652f2
Danny Wei <79378020+Dynnwa@users.noreply.github.com>
2023-06-13 21:39:08 -0700

    redux, default data, collections page displays (#12)

--- backstory/src/components/collections/Collections.js
+++ backstory/src/components/collections/Collections.js
@@ -1,18 +1,17 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./Collections.css";
 import Collection from "../collection/Collection";
+import { useSelector } from 'react-redux';
+import Gallery from '../gallery/Gallery';
 
-function Collections(props) {
-  const collections = [
-    {
-      name: 'Mushrooms',
-      images: ["https://images.unsplash.com/photo-1589539148230-0a5b707cbd7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80", "https://images.unsplash.com/photo-1634159779963-4fafda643dac?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=928&q=80", "https://images.unsplash.com/photo-1618232731737-e1cd67c80bc8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80", "https://images.unsplash.com/photo-1603539007496-0bfa35834541?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=770&q=80"]
-    },
-    /* more collections... */
-  ];
-
+function Collections() {
+  const collections = useSelector((state) => state.collections);
+  const [showCollections, setShowCollections] = useState(true);
+  const [images, setImages] = useState([]);
+  
   return (
     <>
+      {showCollections ? 
       <div className="wrapper">
         <div className="grid">
           {collections.map((collection, index) => (
@@ -20,10 +19,11 @@ function Collections(props) {
               key={index}
               name={collection.name}
               images={collection.images}
+              onSelect={() => {setShowCollections(false);setImages(collection.images)}}
             />
           ))}
         </div>
-      </div>
+      </div> : <Gallery photos={images}/>}
     </>
   );
 }
--- backstory/src/pages/home/Home.js
+++ backstory/src/pages/home/Home.js
@@ -4,16 +4,18 @@ import Navbar from '../../components/navbar/Navbar';
 import Profile from '../../components/profile/Profile';
 import Collections from '../../components/collections/Collections';
 import Gallery from '../../components/gallery/Gallery';
+import { useSelector } from 'react-redux';
 
 function Home() {
   const [activeTab, setActiveTab] = useState("gallery");
+  const items = useSelector((state) => state.photos);
 
   return (
     <div>
       <Navbar />
       <Profile profileImg={"https://media.gq.com/photos/5ba143d4fc6c6260e811638b/1:1/w_1333,h_1333,c_limit/Toad-Alternatives-GQ-2018-091818.jpg"} bio="Already for a mushroom!" name="Toad"/>
       <Tabs setActiveTab={setActiveTab} />
-      {activeTab === "gallery" ? <Gallery /> : <Collections />}
+      {activeTab === "gallery" ? <Gallery photos={items}/> : <Collections />}
     </div>
   );
 }
