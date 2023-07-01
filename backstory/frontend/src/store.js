0d256159373fe793deb225b9c83f6ef54d5879db
Johnny Liao <johnnyliao06@gmail.com>
2023-06-30 17:07:02 -0700

    fixed folder structure in frontend

--- backstory/frontend/src/store.js
+++ backstory/frontend/src/store.js
@@ -1,15 +1,16 @@
 import { collectionsReducer } from './reducers/collections';
 import { configureStore } from '@reduxjs/toolkit';
 import { photosReducer } from './reducers/photos';
+import thunk from 'redux-thunk';
 
 const reducer = {
-    photos: photosReducer,
-    collections: collectionsReducer
-
+  photos: photosReducer,
+  collections: collectionsReducer
 }
 
 const store = configureStore({
-    reducer
+  reducer,
+  middleware: [thunk]
 });
 
 
