7a7b0391cf6f2c117c6d45ed275e2ac1530e3fe9
sharonhe <hesharon0@gmail.com>
2023-08-05 18:32:37 -0700

    Updating Readme and made some fixes to responsiveness and cleaning up code

--- backstory/frontend/src/store.js
+++ backstory/frontend/src/store.js
@@ -1,11 +1,9 @@
 import { collectionsReducer } from './reducers/collections';
 import { configureStore } from '@reduxjs/toolkit';
-import { photosReducer } from './reducers/photos';
 import thunk from 'redux-thunk';
 import userReducer from './slices/user'
 
 const reducer = {
-  photos: photosReducer,
   collections: collectionsReducer,
   user: userReducer
 }
